a86b63a4bc35b36f755682a0a021772c712377d2
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-27 02:03:47 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,16 @@
+// function makealbum (artist: string, title: string): {artist: string; title: string} {
+//     const dictionaries = {
+//         artist: artist.charAt(0).toUpperCase() +artist.slice(1),
+//         title: title.charAt(0).toUpperCase() +title.slice(1)
+//     };
+//     return dictionaries;
+// }
+
+// let album = makealbum("Ali","light")
+// console.log(album)
+
+// album = makealbum("Usama","red wave")
+// console.log(album)
+
+// album = makealbum("Azeem","seecbreez")
+// console.log(album)
